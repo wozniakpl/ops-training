6cadd86f86090f79c032501115172e46c3b17cdb
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2022-11-24 22:34:29 +0100

    key

--- ops/main.tf
+++ ops/main.tf
@@ -17,6 +17,8 @@ resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
+  key_name = aws_key_pair.app_key.key_name
+
   user_data = <<EOF
 #!/bin/bash
 
@@ -25,4 +27,19 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 docker run hello-world
 EOF
+}
+
+resource "aws_key_pair" "app_key" {
+    key_name   = "app_key"
+    public_key = tls_private_key.rsa_key.public_key_openssh
+}
+
+resource "tls_private_key" "rsa_key" {
+    algorithm = "RSA"
+    rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+    filename = "app_key"
+    content  = tls_private_key.rsa_key.private_key_pem
 }
