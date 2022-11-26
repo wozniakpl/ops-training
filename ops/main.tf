bad5dd39ecdc846ea99d4296af3219ea01bc68d4
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2022-11-26 18:51:43 +0100

    SG with ssh

--- ops/main.tf
+++ ops/main.tf
@@ -14,6 +14,26 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-017fecd1353bcc96e"
   instance_type = "t2.micro"
+  key_name = "appkey"
+  vpc_security_group_ids = [ aws_security_group.main.id ]
+}
+
+resource "aws_security_group" "main" {
+  name   = "ssh-sg"
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
